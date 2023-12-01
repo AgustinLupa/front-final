592793a11f8dbe2c8725f061ba83a022c2a832bf
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-12-01 16:52:45 -0300

    Cambio class por className en Base

--- src/Layouts/Base.jsx
+++ src/Layouts/Base.jsx
@@ -5,10 +5,10 @@ const Base = (props) => {
         <>
         <section className="w-[100vw] flex justify-center">
             <menu className="w-[100vw] h-[3rem] absolute bg-slate-500 shadow-sm flex justify-evenly items-center text-white">
-                <div class="dropdown-menu">
-                    <span class="dropdown-item-text">Usuarios</span>
-                        <Link class="dropdown-item" to='/users' replace={true}>Buscar Usuarios</Link>
-                        <Link class="dropdown-item" to='/createUser' replace={true}>Crear Usuarios</Link>
+                <div className="dropdown-menu">
+                    <span className="dropdown-item-text">Usuarios</span>
+                        <Link className="dropdown-item" to='/users' replace={true}>Buscar Usuarios</Link>
+                        <Link className="dropdown-item" to='/createUser' replace={true}>Crear Usuarios</Link>
                 </div>                
             </menu>
             <div className="mt-[4rem]">
