7fa158cf8f2f846f63bfa98afcc2a11fab212f6c
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-12-04 15:45:13 -0300

    Cambio titulo de aplicacion en navbar

--- src/Layouts/Base.jsx
+++ src/Layouts/Base.jsx
@@ -4,7 +4,7 @@ const Base = (props) => {
     return(
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand" href="#">FinalERP</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
