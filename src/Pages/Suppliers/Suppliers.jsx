64170b8526b75462293e10cd1116b02e221a12eb
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-12-04 17:16:25 -0300

    Botones de editar y deshabilitar

--- src/Pages/Suppliers/Suppliers.jsx
+++ src/Pages/Suppliers/Suppliers.jsx
@@ -6,15 +6,28 @@ const Suppliers = (props) => {
     const [supplierData, setSupplierData] = useState({});
     const [tableData, setTableData] = useState([]);
     const [submitErrors, setSubmitErrors] = useState([]);
+    const [itemOnEdit, setItemOnEdit] = useState(null);
  
     const handleSubmit = async (e) => {
         e.preventDefault();
         let rsp = await create(supplierData);
-        if (rsp?.statusCode == 200)
+        if (rsp?.statusCode == 200){
             console.log(rsp);
-        else {
+            loadTableData();
+        }            
+        else if (rsp?.status == 400) {
             console.log(rsp);
         }
+        else {
+            window.alert('No se pudo cargar la informacion Inicie sesion nuevamente');
+            window.location.replace('/login') 
+        }
+    }
+
+    const handleEdit = async (e) => {
+        e.preventDefault();
+        console.log(itemOnEdit);
+        setItemOnEdit(null);
     }
 
     const loadTableData = async () => {
@@ -22,7 +35,13 @@ const Suppliers = (props) => {
         console.log(rsp);
         if (rsp?.statusCode == 200){
             setTableData(rsp.response);
+        } else {
+
         }
+    }
+
+    const hideItem = async (id) => {
+
     }
 
     useEffect( () => {
@@ -36,19 +55,43 @@ const Suppliers = (props) => {
                     <h3 className="text-center mb-4">Proveedores</h3>
                     <div className="card">
                         <div className="card-body">
-                            <h5 className="card-title mb-4">Nuevo proveedor</h5>
-                            <form onSubmit={handleSubmit} className="form-inline">
-                                <label className="sr-only" >Proveedor</label>
-                                <input onChange={(e) => setSupplierData({...supplierData, name: e.target.value})} type="text" className="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="Nombre de Proveedor"/>
+                            {
+                                itemOnEdit === null ? (
+                                    <>
+                                        <h5 className="card-title mb-4">Nuevo proveedor</h5>
+                                        <form onSubmit={handleSubmit} className="form-inline">
+                                            <label className="sr-only" >Proveedor</label>
+                                            <input onChange={(e) => setSupplierData({...supplierData, name: e.target.value})} type="text" className="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="Nombre de Proveedor"/>
 
-                                <label className="sr-only" >Direccion</label>                                                                  
-                                <input onChange={(e) => setSupplierData({...supplierData, address: e.target.value})} type="text" className="form-control mb-2 mr-sm-2" id="inlineFormInputGroupUsername2" placeholder="Direccion"/>      
+                                            <label className="sr-only" >Direccion</label>                                                                  
+                                            <input onChange={(e) => setSupplierData({...supplierData, address: e.target.value})} type="text" className="form-control mb-2 mr-sm-2" id="inlineFormInputGroupUsername2" placeholder="Direccion"/>      
 
-                                <label className="sr-only" >Telefono</label>                                                                  
-                                <input onChange={(e) => setSupplierData({...supplierData, phone: e.target.value})} type="number" className="form-control mb-2 mr-sm-2" id="inlineFormInputGroupUsername2" placeholder="Telefono"/>                                                        
+                                            <label className="sr-only" >Telefono</label>                                                                  
+                                            <input onChange={(e) => setSupplierData({...supplierData, phone: e.target.value})} type="number" className="form-control mb-2 mr-sm-2" id="inlineFormInputGroupUsername2" placeholder="Telefono"/>                                                        
 
-                                <button type="submit" className="btn btn-success mb-2">+</button>
-                            </form>
+                                            <button type="submit" className="btn btn-success mb-2">+</button>
+                                        </form>
+                                    </>                                    
+                                ) : (
+                                    <>
+                                        <h5 className="card-title mb-4">Editar proveedor</h5>
+                                        <form onSubmit={handleEdit} className="form-inline">
+                                            <label className="sr-only" >Cambiar nombre</label>
+                                            <input onChange={(e) => setItemOnEdit({...itemOnEdit, name: e.target.value})} type="text" className="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder={itemOnEdit.name}/>
+
+                                            <label className="sr-only" >Cambiar direccion</label>                                                                  
+                                            <input onChange={(e) => setItemOnEdit({...itemOnEdit, address: e.target.value})} type="text" className="form-control mb-2 mr-sm-2" id="inlineFormInputGroupUsername2" placeholder={itemOnEdit.adress}/>      
+
+                                            <label className="sr-only" >Cambiar telefono</label>                                                                  
+                                            <input onChange={(e) => setItemOnEdit({...itemOnEdit, phone: e.target.value})} type="number" className="form-control mb-2 mr-sm-2" id="inlineFormInputGroupUsername2" placeholder={itemOnEdit.phone}/>                                                        
+
+                                            <button type="submit" className="btn btn-success btn-sm mr-2">Aceptar <i className="bi bi-check2"></i></button>
+                                            <button onClick={() => setItemOnEdit(null)} type="button" className="btn btn-danger btn-sm">Cancelar <i className="bi bi-x-lg"></i></button>
+                                        </form>
+                                    </>
+                                )
+                            }
+                            
                         </div>
                     </div>
                 </div>
@@ -76,6 +119,10 @@ const Suppliers = (props) => {
                                                     <td>{item.name}</td>
                                                     <td>{item.adress}</td>
                                                     <td>{item.phone}</td>
+                                                    <td>
+                                                        <button onClick={() => setItemOnEdit(item)} className="btn btn-sm btn-info mr-2"><i className="bi bi-pencil-square"></i></button>
+                                                        <button onClick={() => hideItem(item.id)} className="btn btn-sm btn-dark"><i className="bi bi-eye-slash"></i></button>
+                                                    </td>
                                                 </tr>
                                             );
                                         })
