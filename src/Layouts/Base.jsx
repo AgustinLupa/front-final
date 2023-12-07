c8ce9b1c612a8f6b0296a836e77405413b886224
agus lupa <agusluparini@gmail.com>
2023-12-06 22:42:09 -0300

    navbar oscuro

--- src/Layouts/Base.jsx
+++ src/Layouts/Base.jsx
@@ -6,7 +6,7 @@ const Base = (props) => {
 
     return(
         <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <a className="navbar-brand" href="#">FinalERP</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
